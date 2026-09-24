c0cb8759337f165481d6ef19379292922e59c557
Your Name <you@example.com>
2026-09-24 10:44:09 +0500

    go

---
+++ 2-masslaa.py
@@ -0,0 +1,9 @@
+import os
+os.system("cls")
+
+a = int(input("uch xonali son kiriting => "))
+birlar_xonasi = a%10
+onlar_xonasi = a%100//10
+yuzlar_xonasi = a%1000//100
+teskar_son = birlar_xonasi*100+onlar_xonasi*10 + yuzlar_xonasi
+print(f"birlar xonasi => {birlar_xonasi}\no'nlar xonasi => {onlar_xonasi}\nyuzlar xonasi => {yuzlar_xonasi}\nberilgan songa tekari son => {teskar_son}")
